9efae42beb21c03000458d5b77f42934852ce72e
VirdaRu <lars.kingma99@gmail.com>
2023-03-27 16:10:34 +0200

    structural repair and error testing

--- CVAnoniem-app/src/app/app.module.ts
+++ CVAnoniem-app/src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { ContactComponent } from './contact/contact.component';
-import { AdminComponent } from './admin/admin.component';
-import { AccountComponent } from './account/account.component';
-import { FAQComponent } from './faq/faq.component';
-import { ErrorComponent } from './global/error/error.component';
-import { CvPageComponent } from './cv-page/cv-page.component';
-import { GlobalComponent } from './global/global.component';
-import { FooterComponent } from './global/footer/footer.component';
-import { HeaderComponent } from './global/header/header.component';
-import { NavbarComponent } from './global/navbar/navbar.component';
-import { InboxComponent } from './account/inbox/inbox.component';
-import { CvUserComponent } from './account/cv-user/cv-user.component';
-import { CVComponent } from './CV/CV.component';
-import { CvListComponent } from './CV/cv-list/cv-list.component';
-import { CvFullComponent } from './CV/cv-full/cv-full.component';
-import { EMailComponent } from './contact/e-mail/e-mail.component';
-import { UserComponent } from './user/user.component';
-import { RecaptchaComponent } from './user/recaptcha/recaptcha.component';
-import { SociaLoginComponent } from './user/social-login/socia-login.component';
-import { SearchComponent } from './home/search/search.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './user/login/login.component';
+import {RegisterComponent} from './user/register/register.component';
+import {ContactComponent} from './contact/contact.component';
+import {AdminComponent} from './admin/admin.component';
+import {AccountComponent} from './account/account.component';
+import {FAQComponent} from './faq/faq.component';
+import {ErrorComponent} from './global/error/error.component';
+import {CvPageComponent} from './cv-page/cv-page.component';
+import {GlobalComponent} from './global/global.component';
+import {FooterComponent} from './global/footer/footer.component';
+import {HeaderComponent} from './global/header/header.component';
+import {NavbarComponent} from './global/navbar/navbar.component';
+import {InboxComponent} from './account/inbox/inbox.component';
+import {CvUserComponent} from './account/cv-user/cv-user.component';
+import {CvComponent} from './cv/cv.component';
+import {CvListComponent} from './cv/cv-list/cv-list.component';
+import {CvFullComponent} from './cv/cv-full/cv-full.component';
+import {EMailComponent} from './contact/e-mail/e-mail.component';
+import {UserComponent} from './user/user.component';
+import {RecaptchaComponent} from './user/recaptcha/recaptcha.component';
+import {SocialLoginComponent} from './user/social-login/social-login.component';
+import {SearchComponent} from './home/search/search.component';
 
 @NgModule({
   declarations: [
@@ -44,13 +44,13 @@ import { SearchComponent } from './home/search/search.component';
     NavbarComponent,
     InboxComponent,
     CvUserComponent,
-    CVComponent,
+    CvComponent,
     CvListComponent,
     CvFullComponent,
     EMailComponent,
     UserComponent,
     RecaptchaComponent,
-    SociaLoginComponent,
+    SocialLoginComponent,
     SearchComponent
   ],
   imports: [
@@ -59,4 +59,5 @@ import { SearchComponent } from './home/search/search.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
