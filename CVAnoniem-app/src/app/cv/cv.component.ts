1c328b444a47c3cd2bb4add39241077073ed3d92
KungFuKevin <kevin.voet@student.nhlstenden.com>
2023-04-11 14:54:04 +0200

    Made CV Component with attributes. Made Login page, to test REST API.

--- CVAnoniem-app/src/app/cv/cv.component.ts
+++ CVAnoniem-app/src/app/cv/cv.component.ts
@@ -1,10 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
   styleUrls: ['./cv.component.css']
 })
-export class CvComponent {
+export class CvComponent{
+  @Input() Branche: string = "";
+  @Input() Beschrijving: string = "";
+  @Input() Provincie: string = "";
 
 }
