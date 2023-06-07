c7b2e36da301e6e43d75825bd724dd3134a76730
VirdaRu <lars.kingma99@gmail.com>
2023-06-07 17:04:07 +0200

    social login

---
+++ CVAnoniem-app/src/app/user/social-login/google-login/google-login.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+import {GoogleApiService, UserInfo} from "./google-api.service";
+
+@Component({
+  selector: 'app-google-login',
+  templateUrl: './google-login.component.html',
+  styleUrls: ['./google-login.component.css']
+})
+export class GoogleLoginComponent {
+  userInfo?: UserInfo
+
+  constructor(private readonly googleApi: GoogleApiService) {
+    googleApi.userProfileSubject.subscribe((info) => {
+      this.userInfo = info
+    })
+  }
+
+  isLoggedIn(): boolean {
+    return this.googleApi.isLoggedIn()
+  }
+
+  logOut(): void {
+    this.googleApi.signOut()
+  }
+}
